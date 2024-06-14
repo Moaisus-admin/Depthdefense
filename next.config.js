727cb1ab77100a4da0021e33938aa5cad9e773b3
SameenKawoosa <sameen@moaisus.com>
2024-06-14 20:15:00 +0530

    Package_json

--- next.config.js
+++ next.config.js
@@ -1,1 +1,12 @@
-// module.exports = { distDir: "out" };
+module.exports = {
+  distDir: "out", // Custom output directory
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.splitChunks = {
+        chunks: "all",
+        maxSize: 250000,
+      };
+    }
+    return config;
+  },
+};
